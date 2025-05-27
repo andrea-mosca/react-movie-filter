45525a8bf4038d0a9bef0c78211dc15b95a4fec0
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-27 15:15:45 +0200

    film list in main

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,3 +1,20 @@
+import films from "../data/films";
 export default function Main() {
-  return;
+  return (
+    <main>
+      <div className="container">
+        <h2>FILMS</h2>
+        <div className="row row-cols-2 g-2">
+          {films.map((film, index) => (
+            <div className="col" key={index}>
+              <div className="card">
+                <div className="card-header">{film.genre}</div>
+                <div className="card-body">{film.title}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
